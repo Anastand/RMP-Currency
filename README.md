b56d19ee3f56d407500994c5da076ef06cd12715
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2025-03-04 15:31:35 +0530

    coudn't complete due to api issue

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RMP-Currency
- 
+Website Link - [RMP-Currency](https://rmp-currency.vercel.app/)
+
+## This project is to learn following
+- [ ] custom hooks and desgin it 
+- [ ] resuable components
+- [ ] working with hooks 
+- [ ] working with API
+
+> code comments explain a lot of things that i understand
+
+## Project - flow / working
+1. 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,79 @@
-import { useState } from 'react'
+import { useState } from "react";
+import InputBox from "./components/InputBox";
+import useCurrnecyInfo from "./hooks/useCurrnecyinfo";
 
 function App() {
-
+  const [amount, setamount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setconvertedAmount] = useState(0);
+  const currencyInfo = useCurrnecyInfo(from);
+  const options = Object.keys(currencyInfo);
+  const convert = () => {
+    setconvertedAmount(amount * currencyInfo[to]);
+  };
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setconvertedAmount(amount);
+    setamount(convertedAmount);
+  };
   return (
-    <>
-      <h1 className='text-4xl text-amber-800'>HEllo world from currency</h1>
-    </>
-  )
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat "
+      style={{
+        backgroundImage: `url(https://img.freepik.com/free-vector/global-business-background-with-stock-chart-blue-tone_53876-117483.jpg?t=st=1741077043~exp=1741080643~hmac=6ccb4934d8206bf8673d1fda0db0faf83ee9359514557f1d70ca7af76493656b&w=740)`,
+      }}
+      // why the style tag has to have two curly brackets -> why write like `url()`
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                onAmountChange={(amount) => setamount(amount)}
+                selectCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox
+                label="To"
+                currencyOptions={options}
+                amount={convertedAmount}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={to}
+                amountDisable
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
